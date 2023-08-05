d7244e15014c8911fba392e1d7b924fbc2b8f228
Abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-08-05 20:16:21 +0300

    tiral

--- app/api/auth/[...nextauth]/options.tsx
+++ app/api/auth/[...nextauth]/options.tsx
@@ -25,8 +25,7 @@ export const options: NextAuthOptions = {
             },
             async authorize(credentials) {
                 const {data: users} = await axios.get(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/users`)
-                console.log(process.env.NEXTAUTH_URL)
-                const authUser = users.find((user: { name: string | undefined }) => user.name === credentials?.username)
+                const authUser = await users.find((user: { name: string | undefined }) => user.name === credentials?.username)
                 if (authUser != null && authUser.password === credentials?.password) {
                     return authUser
                 } else {
