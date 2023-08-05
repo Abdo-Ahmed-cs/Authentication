b1b2fbe67bf7b4e7c2e23062bf595c9ad9f1f12e
Abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-08-05 20:07:36 +0300

    changing an env

--- app/api/auth/[...nextauth]/options.tsx
+++ app/api/auth/[...nextauth]/options.tsx
@@ -24,7 +24,7 @@ export const options: NextAuthOptions = {
                 }
             },
             async authorize(credentials) {
-                const {data: users} = await axios.get(`${process.env.NEXTAUTH_URL}/api/users`)
+                const {data: users} = await axios.get(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/users`)
                 console.log(process.env.NEXTAUTH_URL)
                 const authUser = users.find((user: { name: string | undefined }) => user.name === credentials?.username)
                 if (authUser != null && authUser.password === credentials?.password) {
